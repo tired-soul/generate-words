79e33c6e0c2923938bea0e30e2134bfb37612a86
Nithish <nithishk.ch@gmail.com>
2022-12-10 20:34:50 +0530

    english-dictionary.com is not working; changed it.

--- generate_words.py
+++ generate_words.py
@@ -1,20 +1,27 @@
+from bs4 import BeautifulSoup
 import requests
 
-# URL for the online English dictionary
-url = "https://www.english-dictionary.org/{}"
+# Set the base URL for the website
+base_url = "https://www.dictionary.com/browse/"
 
-# Open the file for writing
+# Open the words.txt file in write mode
 with open("words.txt", "w") as f:
-    # Query for each letter of the alphabet
-    for letter in "abcdefghijklmnopqrstuvwxyz":
-        # Send a GET request to the URL
-        response = requests.get(url.format(letter))
-        # If the request was successful
+    # Loop through the letters of the alphabet
+    for letter in range(ord("a"), ord("z") + 1):
+        # Set the URL for the current letter
+        url = base_url + chr(letter)
+
+        # Make a GET request to the URL
+        response = requests.get(url)
+
+        # Check if the request was successful
         if response.status_code == 200:
-            # Parse the response as HTML
-            html = response.text
-            # Split the HTML into words
-            words = html.split()
+            # Parse the HTML content of the page
+            soup = BeautifulSoup(response.content, "html.parser")
+
+            # Get the list of words on the page
+            words = soup.find("h1", {"class": "css-1sprl0b e1wg9v5m5"}).find_all("li")
+
             # Write each word to the file
             for word in words:
-                f.write(word + "\n")
+                f.write(word.text + "\n")
